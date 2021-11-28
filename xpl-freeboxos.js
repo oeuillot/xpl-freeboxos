2375a268d836d524cc11c899d082e2e009656e18
Olivier Oeuillot <olivier.oeuillot@gmail.com>
2021-11-28 18:10:08 +0100

    Add parental control

--- xpl-freeboxos.js
+++ xpl-freeboxos.js
@@ -11,6 +11,8 @@ const Freebox = require('node-freeboxos');
 
 const version = require("./package.json").version;
 
+const NO_XPL = true;
+
 const LAST_ACTIVITY_INTERVAL_MS = 1000 * 60;
 
 commander.version(version);
@@ -20,68 +22,79 @@ commander.option("-j, --jsonPath <path>", "token JSON path");
 Xpl.fillCommander(commander);
 
 const app = {
-	app_id: "xpl-freebox",
-	app_name: "XPL bridge for Freebox",
-	app_version: version,
-	device_name: "NodeJs/XPL"
+    app_id: "xpl-freebox",
+    app_name: "XPL bridge for Freebox",
+    app_version: version,
+    device_name: "NodeJs/XPL"
 };
 
+const options = commander.opts();
+
 commander.command('run').description("Start pooling freebox").action(
-	() => {
-		console.log("Start");
+    () => {
+        console.log("Start");
 
-		commander.deviceAliases = Xpl.loadDeviceAliases(commander.deviceAliases);
+        options.deviceAliases = Xpl.loadDeviceAliases(options.deviceAliases);
 
-		const config = {app: app};
+        const config = {app: app};
 
-		if (commander.jsonPath) {
-			try {
-				fs.accessSync(commander.jsonPath);
+        if (options.jsonPath) {
+            try {
+                fs.accessSync(options.jsonPath);
 
-				config.jsonPath = commander.jsonPath;
-				config.jsonAutoSave = true;
-			} catch (x) {
-			}
-		}
+                config.jsonPath = options.jsonPath;
+                config.jsonAutoSave = true;
+            } catch (x) {
+            }
+        }
 
-		config.registerDelayMs = 1000 * 60 * 2;
+        config.registerDelayMs = 1000 * 60 * 2;
 
-		var freebox = new Freebox(config);
+        const freebox = new Freebox(config);
 
-		freebox.openSession().then((result) => {
-			debug("Granted result=", result);
+        freebox.openSession().then((result) => {
+            console.log("Freebox granted, result=", result);
 
-			if (!commander.xplSource) {
-				var hostName = os.hostname();
-				if (hostName.indexOf('.') > 0) {
-					hostName = hostName.substring(0, hostName.indexOf('.'));
-				}
 
-				commander.xplSource = "freeboxos." + hostName;
-			}
+            if (NO_XPL) {
+                setInterval(() => poolFreebox(freebox, xpl), 1000 * 5);
 
-			var xpl = new Xpl(commander);
+            } else {
+                if (!options.xplSource) {
+                    var hostName = os.hostname();
+                    if (hostName.indexOf('.') > 0) {
+                        hostName = hostName.substring(0, hostName.indexOf('.'));
+                    }
 
-			xpl.on("error", (error) => {
-				console.log("XPL error", error);
-			});
+                    options.xplSource = "freeboxos." + hostName;
+                }
 
-			xpl.bind((error) => {
-				if (error) {
-					console.error(error);
-					return;
-				}
+                var xpl = new Xpl(options);
 
-				setInterval(() => poolFreebox(freebox, xpl), 1000 * 5);
-			});
-		});
+                xpl.on("error", (error) => {
+                    console.log("XPL error", error);
+                });
 
-	});
+                xpl.bind((error) => {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
+
+                    setInterval(() => poolFreebox(freebox, xpl), 1000 * 5);
+                });
+            }
+        });
+
+    });
 commander.parse(process.argv);
 
 var currentHosts = {};
 var intervalHosts = {};
 var lastIntervalDate = 0;
+
+var currentFilters = {};
+var default_filter_mode;
 
 /**
  *
@@ -89,127 +102,194 @@ var lastIntervalDate = 0;
  * @param {Xpl} xpl
  */
 function poolFreebox(freebox, xpl) {
-	debug("Pool freebox");
+    console.log("Pool freebox");
 
-	freebox.lanBrowser().then((result) => {
-		result = result.filter((v) => v.primary_name);
-		var filtredResult = result.reduce((prev, v) => {
-			var lastActivity = v.last_activity ? (new Date(v.last_activity * 1000)) : undefined;
-			var lastReachable = v.last_time_reachable ? (new Date(v.last_time_reachable * 1000)) : undefined;
+    freebox.getParentalConfig().then((result) => {
+        console.log('Parental Config=', result);
 
-			prev[v.primary_name] = {enabled: v.reachable, lastActivity, lastReachable};
-			return prev;
-		}, {});
+        if (default_filter_mode===result.default_filter_mode) {
+            return;
+        }
+        default_filter_mode=result.default_filter_mode;
 
-		var oldHosts = currentHosts;
-		currentHosts = filtredResult;
-		var messages = [];
+        const message= {
+            device: 'parental-rule/default',
+            current: default_filter_mode,
+        }
+        xpl.sendXplStat(message, "sensor.basic", (error)=> {
+            if (error) {
+                console.error(error);
+                return;
+            }
 
-		for (var k in filtredResult) {
-			var cur = filtredResult[k];
-			var old = oldHosts[k];
-			var hostName = 'host/' + k.replace(/\//g, '_');
-			if (!old) {
-				// Un nouveau !!!
+            debug("send", "Parental config messages sent !");
+        });
+    });
 
-				messages.push({
-					device: hostName + '/reachable',
-					current: cur.enabled
-				});
-				if (cur.lastActivity) {
-					intervalHosts[hostName] = cur;
-					messages.push({
-						device: hostName + '/lastActivity',
-						current: cur.lastActivity.toISOString()
-					});
-				}
-				continue;
-			}
 
-			delete oldHosts[k];
+    freebox.getParentalFilter().then((result) => {
+        console.log('Parental filters=', result);
 
-			if (cur.enabled !== old.enabled) {
-				var en = {
-					device: hostName + '/reachable',
-					current: cur.enabled
-				};
-				if (!cur.enabled && cur.lastReachable) {
-					en.date = cur.lastReachable.toISOString();
-				}
-				messages.push(en);
-				if (intervalHosts[hostName]) {
-					messages.push({
-						device: hostName + '/lastActivity',
-						current: cur.lastActivity.toISOString()
-					});
-					delete intervalHosts[hostName];
-					continue;
-				}
-			}
-			if (cur.lastActivity && (!old.lastActivity || cur.lastActivity.getTime() !== old.lastActivity.getTime())) {
-				intervalHosts[hostName] = cur;
-			}
-		}
+        const messages = [];
+        result.forEach((f) => {
+            const {forced, forced_mode, desc} = f;
 
-		for (var k in oldHosts) {
-			var old = oldHosts[k];
-			var hostName = 'host/' + k.replace(/\//g, '_');
+            const prev = currentFilters[desc];
+            if (prev) {
+                if (prev.forced === forced && prev.forced_mode === forced_mode) {
+                    return;
+                }
+            }
 
-			var en = {
-				device: hostName + '/reachable',
-				current: false,
-				lost: true
-			};
-			if (old.lastReachable) {
-				en.date = old.lastReachable.toISOString();
-			}
-			messages.push(en);
+            currentFilters[desc] = f;
 
-			if (intervalHosts[hostName]) {
-				messages.push({
-					device: hostName + '/lastActivity',
-					current: old.lastActivity.toISOString()
-				});
-				delete intervalHosts[hostName];
-				continue;
-			}
-		}
+            messages.push({
+                device: 'parental-rule/' + desc,
+                current: forced ? (forced_mode ? 'true' : 'false') : 'planning',
+            });
 
-		var now = Date.now();
-		if (lastIntervalDate + LAST_ACTIVITY_INTERVAL_MS < now) {
-			lastIntervalDate = now;
+            if (xpl) {
+                debug("Send", messages.length, "messages.");
 
-			for (var k in intervalHosts) {
-				messages.push({
-					device: k + '/lastActivity',
-					current: intervalHosts[k].lastActivity.toISOString()
-				});
-			}
-			intervalHosts = {};
-		}
+                async.eachSeries(messages, (message, callback) => {
+                    debug("Send message=", message);
 
-		debug("Send", messages.length, "messages.");
+                    xpl.sendXplStat(message, "sensor.basic", callback);
 
-		async.eachSeries(messages, (message, callback) => {
-			debug("Send message=", message);
+                }, (error) => {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
 
-			xpl.sendXplStat(message, "sensor.basic", callback);
+                    debug("send", messages.length + " messages sent !");
+                });
+            }
+        });
 
-		}, (error) => {
-			if (error) {
-				console.error(error);
-				return;
-			}
+    });
 
-			debug("send", messages.length + " messages sent !");
-		});
+    freebox.lanBrowser().then((result) => {
+        result = result.filter((v) => v.primary_name);
+        var filtredResult = result.reduce((prev, v) => {
+            var lastActivity = v.last_activity ? (new Date(v.last_activity * 1000)) : undefined;
+            var lastReachable = v.last_time_reachable ? (new Date(v.last_time_reachable * 1000)) : undefined;
 
-	}).catch((error) => {
-		console.error(error);
-	})
+            prev[v.primary_name] = {enabled: v.reachable, lastActivity, lastReachable};
+            return prev;
+        }, {});
+
+        var oldHosts = currentHosts;
+        currentHosts = filtredResult;
+        var messages = [];
+
+        for (var k in filtredResult) {
+            var cur = filtredResult[k];
+            var old = oldHosts[k];
+            var hostName = 'host/' + k.replace(/\//g, '_');
+            if (!old) {
+                // Un nouveau !!!
+
+                messages.push({
+                    device: hostName + '/reachable',
+                    current: cur.enabled
+                });
+                if (cur.lastActivity) {
+                    intervalHosts[hostName] = cur;
+                    messages.push({
+                        device: hostName + '/lastActivity',
+                        current: cur.lastActivity.toISOString()
+                    });
+                }
+                continue;
+            }
+
+            delete oldHosts[k];
+
+            if (cur.enabled !== old.enabled) {
+                var en = {
+                    device: hostName + '/reachable',
+                    current: cur.enabled
+                };
+                if (!cur.enabled && cur.lastReachable) {
+                    en.date = cur.lastReachable.toISOString();
+                }
+                messages.push(en);
+                if (intervalHosts[hostName]) {
+                    messages.push({
+                        device: hostName + '/lastActivity',
+                        current: cur.lastActivity.toISOString()
+                    });
+                    delete intervalHosts[hostName];
+                    continue;
+                }
+            }
+            if (cur.lastActivity && (!old.lastActivity || cur.lastActivity.getTime() !== old.lastActivity.getTime())) {
+                intervalHosts[hostName] = cur;
+            }
+        }
+
+        for (var k in oldHosts) {
+            var old = oldHosts[k];
+            var hostName = 'host/' + k.replace(/\//g, '_');
+
+            var en = {
+                device: hostName + '/reachable',
+                current: false,
+                lost: true
+            };
+            if (old.lastReachable) {
+                en.date = old.lastReachable.toISOString();
+            }
+            messages.push(en);
+
+            if (intervalHosts[hostName]) {
+                messages.push({
+                    device: hostName + '/lastActivity',
+                    current: old.lastActivity.toISOString()
+                });
+                delete intervalHosts[hostName];
+                continue;
+            }
+        }
+
+        var now = Date.now();
+        if (lastIntervalDate + LAST_ACTIVITY_INTERVAL_MS < now) {
+            lastIntervalDate = now;
+
+            for (var k in intervalHosts) {
+                messages.push({
+                    device: k + '/lastActivity',
+                    current: intervalHosts[k].lastActivity.toISOString()
+                });
+            }
+            intervalHosts = {};
+        }
+
+        if (xpl) {
+            debug("Send", messages.length, "messages.");
+
+            async.eachSeries(messages, (message, callback) => {
+                debug("Send message=", message);
+
+                xpl.sendXplStat(message, "sensor.basic", callback);
+
+            }, (error) => {
+                if (error) {
+                    console.error(error);
+                    return;
+                }
+
+                debug("send", messages.length + " messages sent !");
+            });
+        }
+
+    }).catch((error) => {
+        console.error(error);
+    })
 }
 
 if (commander.headDump) {
-	var heapdump = require("heapdump");
-	console.log("***** HEAPDUMP enabled **************");
+    var heapdump = require("heapdump");
+    console.log("***** HEAPDUMP enabled **************");
 }
